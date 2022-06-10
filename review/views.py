82ddd00e17adab43365f5541a8b4944dce007535
vionaosiako <viona.osiako@student.morigaschool.com>
2022-06-10 23:50:28 +0300

    Craete  registrationPage and loginPage functions

--- review/views.py
+++ review/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from .forms import  CreateUserForm
 
 # Create your views here.
+def registerPage(request):
+    form =  CreateUserForm()
+    contex = {'form':form}
+    if request.method == 'POST':
+        form= CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('loginPage')
+    return render(request, 'auth/register.html', contex)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+    contex = {}
+    return render(request, 'auth/login.html', contex)
+
 def index(request):
     context={}
     return render(request, 'index.html',context)
